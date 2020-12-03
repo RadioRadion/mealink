31573343e317ad5440c3ab6c9ce0885b597e7b85
Valentin Lassartesse <valentin.lassartesse@gmail.com>
2020-12-03 09:57:22 +0100

    crud @ namespace cooker done for meals

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,9 @@ Rails.application.routes.draw do
   end
   resources :meals, only: [ :index, :show ]
 
+  namespace :cooker do
+    resources :meals
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
